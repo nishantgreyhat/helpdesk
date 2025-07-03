c0b82b4d25c3aea2a9c8d500a210428781c823fa
nishantgreyhat <nishantmishra8528@gmail.com>
2025-07-03 13:59:26 +0530

    Fix: added base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Add this line to fix blank screen on Netlify
 })
